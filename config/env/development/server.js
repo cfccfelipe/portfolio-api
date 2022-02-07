5ea639a27f2021536d7749a696a45162807ee7ed
cfccfelipe <cfccfelipe@gmail.com>
2022-02-07 11:31:35 -0500

    version 0.1

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8ba0e000385a0529c8bf09ec7a3d4237'),
+      secret: env("ADMIN_JWT_SECRET", "8ba0e000385a0529c8bf09ec7a3d4237"),
     },
   },
 });
